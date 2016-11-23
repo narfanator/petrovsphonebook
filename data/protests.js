683b7c8cb54eddaaae6cb71484b576ba9b0f8b24
Nick Barone <narfanator@gmail.com>
2016-11-22 20:37:50 -0800

    Tweak seed data

--- data/protests.js
+++ data/protests.js
@@ -3,36 +3,30 @@ const protests = [
       "name":"Standing Rock",
       "jurisdiction": ["United States", "North Dakota", "County", "Collbone"],
       "website": "https://nodaplsolidarity.org",
-      "id":"1161022966406956503",
-      "contacts": {
-        "United States": [
-          ["White House","<fb link>","<phone line>", "people":[]]
-        ],
-        "North Dakota": [
-          ["Governer's Office", "<fb link>", "<phone line>", "people":[]],
-          ["State Troopers", "<fb link>", "<phone line>", "people":[]],
-          ["National Guard", "<phone line>", "people":[]],
-        ],
-        "ACLU": ["..."]
-      }
+      "contacts": [
+        {"name": "United States",
+        "list": [
+          { "name": "White House",
+            "facebook": "<fb link>",
+            "phone": "<phone line>",
+            "people":[]
+          }
+        ]},
+        {"name": "North Dakota",
+        "list": [
+          {"name":"Governer's Office", "facebook":"<fb link>", "phone": "<phone line>", "people":[]},
+          {"name": "State Troopers", "facebook":"<fb link>", "phone":"<phone line>", "people":[]},
+          {"name":"National Guard", "phone":"<phone line>", "people":[]},
+        ]}
+      ]
    },
    {
      //TODO: When localizing, also have how jurisdiction list turns into human name - in the US, it's City, State
       "name": "Occupy LA",
       "jurisdiction": ["United States", "California", "Los Angeles", "Los Angeles"],
       "website": "http://occupyla.org/",
-      "id":"1160844458347054781",
-      "contacts": {
-        "United States": [
-          ["White House","<fb link>","<phone line>", "people":[]]
-        ],
-        "North Dakota": [
-          ["Governer's Office", "<fb link>", "<phone line>", "people":[]],
-          ["State Troopers", "<fb link>", "<phone line>", "people":[]],
-          ["National Guard", "<phone line>", "people":[]],
-        ],
-        "ACLU": ["..."]
-      }
+      "contacts": [
+      ]
     }
 ];
 
